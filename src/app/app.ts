97d70d372414e94a56ccd4eeb0597f25892a3065
GUIBERT Vincent <vincent.guibert@externe.maif.fr>
2026-05-12 16:22:35 +0200

    feat : composant rating star

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Stars } from "./shared/stars/stars";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Stars],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
